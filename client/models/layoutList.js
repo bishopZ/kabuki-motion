6b8bc9f1a73b3c4718c4dba934b3e777b3921928
bishopz <xchicago@gmail.com>
2015-11-28 13:18:36 -0700

    adding redux

--- client/models/layoutList.js
+++ client/models/layoutList.js
@@ -1,6 +1,6 @@
-import NotFound from 'layouts/notFound';
-import Grid from 'layouts/grid';
-import FullScreen from 'layouts/fullScreen';
+import NotFound from '../layouts/notFound';
+import Grid from '../layouts/grid';
+import FullScreen from '../layouts/fullScreen';
 
 var Layouts = {
   NotFound: NotFound,
